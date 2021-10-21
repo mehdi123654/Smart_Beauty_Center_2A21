85c0b07a4825bb6cbd0b2dadb280ab4e4cfd1c40
mehdi fathallah <mehdi.fathallah@esprit.tn>
2021-10-22 00:36:20 +0100

    First Commit

---
+++ connection.cpp
@@ -0,0 +1,23 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("test2-bd");
+db.setUserName("mehdi");//inserer nom de l'utilisateur
+db.setPassword("mehdi");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
